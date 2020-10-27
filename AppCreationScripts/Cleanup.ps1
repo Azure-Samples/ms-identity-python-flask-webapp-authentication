2a983d7675070d035ea4a9711f28012abbb27ee2
idg-sam <66391663+idg-sam@users.noreply.github.com>
2020-10-27 04:19:14 -0700

    app name powershell

--- AppCreationScripts/Cleanup.ps1
+++ AppCreationScripts/Cleanup.ps1
@@ -59,9 +59,9 @@ Function Cleanup
     # Removes the applications
     Write-Host "Cleaning-up applications from tenant '$tenantName'"
 
-    Write-Host "Removing 'webApp' (WebApp_Python_Flask_Authentication_MyOrg) if needed"
-    Get-AzureADApplication -Filter "DisplayName eq 'WebApp_Python_Flask_Authentication_MyOrg'"  | ForEach-Object {Remove-AzureADApplication -ObjectId $_.ObjectId }
-    $apps = Get-AzureADApplication -Filter "DisplayName eq 'WebApp_Python_Flask_Authentication_MyOrg'"
+    Write-Host "Removing 'webApp' (webapp-python-flask-auth-my-tenant) if needed"
+    Get-AzureADApplication -Filter "DisplayName eq 'webapp-python-flask-auth-my-tenant'"  | ForEach-Object {Remove-AzureADApplication -ObjectId $_.ObjectId }
+    $apps = Get-AzureADApplication -Filter "DisplayName eq 'webapp-python-flask-auth-my-tenant'"
     if ($apps)
     {
         Remove-AzureADApplication -ObjectId $apps.ObjectId
@@ -70,10 +70,10 @@ Function Cleanup
     foreach ($app in $apps) 
     {
         Remove-AzureADApplication -ObjectId $app.ObjectId
-        Write-Host "Removed WebApp_Python_Flask_Authentication_MyOrg.."
+        Write-Host "Removed webapp-python-flask-auth-my-tenant.."
     }
     # also remove service principals of this app
-    Get-AzureADServicePrincipal -filter "DisplayName eq 'WebApp_Python_Flask_Authentication_MyOrg'" | ForEach-Object {Remove-AzureADServicePrincipal -ObjectId $_.Id -Confirm:$false}
+    Get-AzureADServicePrincipal -filter "DisplayName eq 'webapp-python-flask-auth-my-tenant'" | ForEach-Object {Remove-AzureADServicePrincipal -ObjectId $_.Id -Confirm:$false}
     
 }
 
--- AppCreationScripts/Configure.ps1
+++ AppCreationScripts/Configure.ps1
@@ -187,17 +187,17 @@ Function ConfigureApplications
     $user = Get-AzureADUser -ObjectId $creds.Account.Id
 
    # Create the webApp AAD application
-   Write-Host "Creating the AAD application (WebApp_Python_Flask_Authentication_MyOrg)"
+   Write-Host "Creating the AAD application (webapp-python-flask-auth-my-tenant)"
    # Get a 2 years application key for the webApp Application
    $pw = ComputePassword
    $fromDate = [DateTime]::Now;
    $key = CreateAppKey -fromDate $fromDate -durationInYears 2 -pw $pw
    $webAppAppKey = $pw
    # create the application 
-   $webAppAadApplication = New-AzureADApplication -DisplayName "WebApp_Python_Flask_Authentication_MyOrg" `
+   $webAppAadApplication = New-AzureADApplication -DisplayName "webapp-python-flask-auth-my-tenant" `
                                                   -HomePage "https://127.0.0.1:5000" `
                                                   -ReplyUrls "https://127.0.0.1:5000/auth/redirect" `
-                                                  -IdentifierUris "https://$tenantName/WebApp_Python_Flask_Authentication_MyOrg" `
+                                                  -IdentifierUris "https://$tenantName/webapp-python-flask-auth-my-tenant" `
                                                   -PasswordCredentials $key `
                                                   -PublicClient $False
 
@@ -214,12 +214,12 @@ Function ConfigureApplications
    }
 
 
-   Write-Host "Done creating the webApp application (WebApp_Python_Flask_Authentication_MyOrg)"
+   Write-Host "Done creating the webApp application (webapp-python-flask-auth-my-tenant)"
 
    # URL of the AAD application in the Azure portal
    # Future? $webAppPortalUrl = "https://portal.azure.com/#@"+$tenantName+"/blade/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/Overview/appId/"+$webAppAadApplication.AppId+"/objectId/"+$webAppAadApplication.ObjectId+"/isMSAApp/"
    $webAppPortalUrl = "https://portal.azure.com/#blade/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/CallAnAPI/appId/"+$webAppAadApplication.AppId+"/objectId/"+$webAppAadApplication.ObjectId+"/isMSAApp/"
-   Add-Content -Value "<tr><td>webApp</td><td>$currentAppId</td><td><a href='$webAppPortalUrl'>WebApp_Python_Flask_Authentication_MyOrg</a></td></tr>" -Path createdApps.html
+   Add-Content -Value "<tr><td>webApp</td><td>$currentAppId</td><td><a href='$webAppPortalUrl'>webapp-python-flask-auth-my-tenant</a></td></tr>" -Path createdApps.html
 
    $requiredResourcesAccess = New-Object System.Collections.Generic.List[Microsoft.Open.AzureAD.Model.RequiredResourceAccess]
 
@@ -227,7 +227,7 @@ Function ConfigureApplications
    Write-Host "Granted permissions."
 
    # Update config file for 'webApp'
-   $configFile = $pwd.Path + "\..\config.py"
+   $configFile = $pwd.Path + "\..\aad.config.json"
    Write-Host "Updating the sample code ($configFile)"
    $dictionary = @{ "default-value-enter-your-tenant-id-here" = $tenantId;"default-value-enter-your-client-id-here" = $webAppAadApplication.AppId;"default-value-enter-your-client-secret-here" = $webAppAppKey };
    ReplaceInTextFile -configFilePath $configFile -dictionary $dictionary
